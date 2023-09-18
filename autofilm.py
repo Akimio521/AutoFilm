f7f4bb398ee3dc597cde382978fea36ae8f4d630
zui-lang <77880495+zui-lang@users.noreply.github.com>
2023-09-18 21:59:14 +0800

    更新autofilm.py：新增三个参数，可选字幕文件、图片文件、视频信息文件是否下载

--- autofilm.py
+++ autofilm.py
@@ -1,11 +1,13 @@
 from webdav3.client import Client
-import argparse,os,requests,time
+import argparse, os, requests, time
 
-# 如果depth为None，则会递归遍历整个WebDAV服务器
-# 如果depth为正整数，则会递归遍历到指定深度
-# 如果depth为0，则只会遍历当前文件夹中的文件和文件夹，不会继续递归遍历下一级文件夹。
+'''
+遍历Webdav服务器函数
+如果depth为None，则会递归遍历整个WebDAV服务器
+如果depth为正整数，则会递归遍历到指定深度
+如果depth为0，则只会遍历当前文件夹中的文件和文件夹，不会继续递归遍历下一级文件夹。
+'''
 def list_files(webdav_url, username, password, depth=None, path=''):
-    # 创建WebDAV客户端
     options = {
         'webdav_hostname': webdav_url,
         'webdav_login': username,
@@ -18,7 +20,6 @@ def list_files(webdav_url, username, password, depth=None, path=''):
     q = 1
     while q < 15:
         try:
-            # 获取WebDAV服务器上的文件列表
             items = client.list()
         except:
             print(f'第{q}次连接失败，{q+1}秒后重试...')
@@ -35,7 +36,6 @@ def list_files(webdav_url, username, password, depth=None, path=''):
 
     for item in items[1:]:
         if item[-1] == '/':
-            # 如果是文件夹，则递归遍历其中的文件和文件夹
             if depth is None or depth > 0:
                 subdirectory, subfiles = list_files(webdav_url + item, username, password, depth=None if depth is None else depth - 1, path=path+item)
                 directory += [item + subitem for subitem in subdirectory]
@@ -48,16 +48,40 @@ def list_files(webdav_url, username, password, depth=None, path=''):
         print(f'当前文件夹路径：{path}')
     return directory, files
 
+'''
+下载函数
+用于'ASS', 'SRT', 'SSA','NFO','JPG', 'PNG'文件的下载
+'''   
+def download_file(url, local_path, filename):
+    p = 1
+    while p < 10:
+        try:
+            print('正在下载：' + filename)
+            r = requests.get(url.replace('/dav', '/d'))
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+            with open(local_path, 'wb') as f:
+                f.write(r.content)
+                f.close
+        except:
+            print(f'第{p}次下载失败，{p + 1}秒后重试...')
+            p += 1
+            time.sleep(p)
+        else:
+            if p > 1:
+                print('重新下载成功！')
+            print(filename + '下载成功！')
+            break
 
-# 解析命令行参数
 parser = argparse.ArgumentParser(description='Autofilm script')
 parser.add_argument('--webdav_url', type=str, help='WebDAV服务器地址', required=True)
 parser.add_argument('--username', type=str, help='WebDAV账号', required=True)
 parser.add_argument('--password', type=str, help='WebDAV密码', required=True)
 parser.add_argument('--output_path', type=str, help='输出文件目录', required=True)
+parser.add_argument('--subtitle', type=str, help='是否下载字幕文件', choices=['true', 'false'], default='true')
+parser.add_argument('--nfo', type=str, help='是否下载NFO文件', choices=['true', 'false'], default='false')
+parser.add_argument('--img', type=str, help='是否下载JPG和PNG文件', choices=['true', 'false'], default='false')
 args = parser.parse_args()
 
-# 调用函数获取文件列表并保存到本地
 directory = list_files(args.webdav_url, args.username, args.password, depth=None, path='')[0]
 files = list_files(args.webdav_url, args.username, args.password, depth=None, path='')[1]
 
@@ -68,7 +92,9 @@ for url in urls:
         continue
     filename = os.path.basename(url)
     local_path = os.path.join(args.output_path, url.replace(args.webdav_url, '').lstrip('/'))
-    if filename[-3:].upper() in ['MP4', 'MKV', 'FLV', 'AVI']:
+    file_ext = filename[-3:].upper()
+
+    if file_ext in ['MP4', 'MKV', 'FLV', 'AVI']:
         if not os.path.exists(os.path.join(args.output_path, filename[:-3] + 'strm')):
             print('正在处理：' + filename)
             try:
@@ -77,24 +103,14 @@ for url in urls:
                     f.write(url.replace('/dav', '/d'))
             except:
                 print(filename + '处理失败，文件名包含特殊符号，建议重命名！')
-    elif filename[-3:].upper() in ['ASS', 'SRT', 'SSA', 'JPG', 'NFO']:
+    elif args.subtitle == 'true' and file_ext in ['ASS', 'SRT', 'SSA']:
         if not os.path.exists(local_path):
-            p = 1
-            while p < 10:
-                try:
-                    print('正在下载：' + filename)
-                    r = requests.get(url.replace('/dav', '/d'))
-                    os.makedirs(os.path.dirname(local_path), exist_ok=True)
-                    with open(local_path, 'wb') as f:
-                        f.write(r.content)
-                        f.close
-                except:
-                    print(f'第{p}次下载失败，{p + 1}秒后重试...')
-                    p += 1
-                    time.sleep(p)
-                else:
-                    if p > 1:
-                        print('重新下载成功！')
-                    break
+            download_file(url, local_path, filename)
+    elif args.nfo == 'true' and file_ext == 'NFO':
+        if not os.path.exists(local_path):
+            download_file(url, local_path, filename)
+    elif args.img == 'true' and file_ext in ['JPG', 'PNG']:
+        if not os.path.exists(local_path):
+            download_file(url, local_path, filename)
 
 print('处理完毕！')
