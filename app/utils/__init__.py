37fc99ab5349974a4e6e0c2b3fb410076cbb5364
Akimio521 <akimio2333@gmail.com>
2024-07-11 15:38:24 +0800

    feat(retry):重试装饰器同时支持同步函数和异步函数

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,2 @@
 from app.utils.urltree import structure_to_dict, dict_to_structure
-from app.utils.retry import retry, async_retry
+from app.utils.retry import retry
--- app/utils/retry.py
+++ app/utils/retry.py
@@ -2,9 +2,9 @@
 重试装饰器
 """
 
-import asyncio
-import inspect
-from typing import Any, TypeVar
+from asyncio import sleep as async_sleep
+from inspect import iscoroutinefunction
+from typing import Any, TypeVar, Callable
 from time import sleep
 from logging import Logger
 
@@ -14,6 +14,8 @@ TRIES = 3
 DELAY = 3
 BACKOFF = 1
 
+T = TypeVar("T")
+
 
 def retry(
     ExceptionToCheck: Any,
@@ -21,10 +23,10 @@ def retry(
     delay: int = DELAY,
     backoff: int = BACKOFF,
     logger: LoggerManager | Logger | None = None,
-    ret: Any = None,
+    ret: T = None,
 ):
     """
-    同步步重试装饰器
+    同步/异步重试装饰器
 
     :param ExceptionToCheck: 需要捕获的异常
     :param tries: 重试次数
@@ -34,70 +36,46 @@ def retry(
     :param ret: 默认返回
     """
 
-    def deco_retry(f):
-        def f_retry(*args, **kwargs) -> Any:
+    def deco_retry(f: Callable[..., T]) -> Callable[..., T]:
+        async def async_wrapper(*args, **kwargs) -> T:
+            return await f(*args, **kwargs)
+
+        def sync_wrapper(*args, **kwargs) -> T:
+            return f(*args, **kwargs)
+
+        async def retry_logic(wrapper, *args, **kwargs) -> T:
             mtries, mdelay = tries, delay
-            while mtries > 0:
+            while mtries > 1:
                 try:
-                    return f(*args, **kwargs)
+                    return await wrapper(*args, **kwargs)
                 except ExceptionToCheck as e:
                     msg = f"出现错误，错误信息{e}，{mdelay}秒后重试 ..."
                     if logger:
                         logger.warning(msg)
                     else:
                         print(msg)
-                    sleep(mdelay)
-                    mtries -= 1
-                    mdelay *= backoff
-            if logger:
-                logger.warning("超出最大重试次数！返回默认值")
-            return ret
 
-        return f_retry
-
-    return deco_retry
-
-
-T = TypeVar("T")
-
-
-def async_retry(
-    ExceptionToCheck: Any,
-    tries: int = TRIES,
-    delay: int = DELAY,
-    backoff: int = BACKOFF,
-    logger: LoggerManager | Logger | None = None,
-    ret: Any = None,
-):
-    """
-    异步重试装饰器
-
-    :param ExceptionToCheck: 需要捕获的异常
-    :param tries: 重试次数
-    :param delay: 延迟时间
-    :param backoff: 延迟倍数
-    :param logger: 日志对象（LoggerManager | logger）
-    :param ret: 默认返回
-    """
-
-    def deco_retry(f):
-        async def f_retry(*args, **kwargs) -> T:
-            mtries, mdelay = tries, delay
-            while mtries > 0:
-                try:
-                    return await f(*args, **kwargs)
-                except ExceptionToCheck as e:
-                    msg = f"出现错误，错误信息{e}，{mdelay}秒后重试 ..."
-                    if logger:
-                        logger.warning(msg)
+                    if iscoroutinefunction(f):
+                        await async_sleep(mdelay)
                     else:
-                        print(msg)
-                    await asyncio.sleep(mdelay)
+                        sleep(mdelay)
+
                     mtries -= 1
                     mdelay *= backoff
+
             if logger:
                 logger.warning("超出最大重试次数！返回默认值")
             return ret
+
+        if iscoroutinefunction(f):
+
+            async def f_retry(*args, **kwargs) -> T:
+                return await retry_logic(async_wrapper, *args, **kwargs)
+
+        else:
+
+            async def f_retry(*args, **kwargs) -> T:
+                return await retry_logic(sync_wrapper, *args, **kwargs)
 
         return f_retry
 
