40754792f3a752710489bcedc263a766c1744930
Akimio521 <akimio2333@gmail.com>
2024-07-12 15:17:32 +0800

    perfect(ani2alist):优化判断逻辑

--- app/modules/ani2alist/ani2alist.py
+++ app/modules/ani2alist/ani2alist.py
@@ -31,7 +31,7 @@ class Ani2Alist:
         username: str = "",
         password: str = "",
         target_dir: str = "/Anime",
-        latest: bool = True,
+        rss_update: bool = True,
         year: int | None = None,
         month: int | None = None,
         src_domain: str = "aniopen.an-i.workers.dev",
@@ -45,38 +45,56 @@ class Ani2Alist:
         :param origin: Alist 服务器地址，默认为 "http://localhost:5244"
         :param username: Alist 用户名，默认为空
         :param password: Alist 密码，默认为空
-        :param target_dir: 挂载到 Alist 服务器上目录，默认为 "/"
-        :param latest: 使用RSS追更最新番剧，默认为 True
+        :param target_dir: 挂载到 Alist 服务器上目录，默认为 "/Anime"
+        :param rss_update: 使用RSS追更最新番剧，默认为 True
         :param year: 动画年份，默认为空
         :param month: 动画季度，默认为空
         :param src_domain: ANI Open 项目地址，默认为 "aniopen.an-i.workers.dev"，可自行反代
         :param rss_domain ANI Open 项目 RSS 地址，默认为 "api.ani.rip"，可自行反代
         :param key_word: 自定义关键字，默认为空
         """
+
+        def is_time_valid(self, year: int, month: int) -> bool:
+            """
+            判断时间是否合理
+            """
+            current_date = datetime.now()
+            if (year, month) == (2019, 4):
+                logger.warning("2019-4季度暂无数据")
+                return False
+            elif (year, month) < (2019, 1):
+                logger.warning("ANI Open项目仅支持2019年1月及其之后的数据")
+                return False
+            elif (year, month) > (current_date.year, current_date.month):
+                logger.warning("传入的年月晚于当前时间")
+                return False
+            else:
+                return True
+
         self.__url = url
         self.__username = username
         self.__password = password
         self.__target_dir = "/" + target_dir.strip("/")
-        self.__latest = latest
 
         self.__year = None
         self.__month = None
         self.__key_word = None
+        self.__rss_update = rss_update
 
-        if self.__latest:
-            if self.__is_valid(year, month, key_word):
+        if rss_update:
+            logger.debug("使用RSS追更最新番剧")
+        elif key_word:
+            logger.debug(f"使用自定义关键字：{key_word}")
+            self.__key_word = key_word
+        elif year and month:
+            if is_time_valid(year, month):
+                logger.debug(f"传入季度：{year}-{month}")
                 self.__year = year
                 self.__month = month
-                self.__key_word = key_word
-                if key_word is None:
-                    logger.debug(f"传入时间{year}-{month}")
-                else:
-                    logger.debug(f"传入关键字{key_word}")
-            else:
-                if year is None and month is None:
-                    logger.debug("未传入时间，将使用当前时间")
-                else:
-                    logger.warning(f"传入时间{year}-{month}不合理，将使用当前时间")
+        elif year or month:
+            logger.warning("未传入完整时间参数，默认使用当前季度")
+        else:
+            logger.debug("未传入时间参数，默认使用当前季度")
 
         self.__src_domain = src_domain.strip()
         self.__rss_domain = rss_domain.strip()
@@ -95,7 +113,7 @@ class Ani2Alist:
         current_season = self.__get_ani_season
         logger.info(f"开始更新ANI Open{current_season}季度番剧")
 
-        if self.__latest:
+        if self.__rss_update:
             anime_dict = await self.get_rss_anime_dict
         else:
             anime_dict = await self.get_season_anime_list
@@ -144,45 +162,16 @@ class Ani2Alist:
             else:
                 logger.error(f"创建存储器后未找到存储器：{self.__target_dir}")
 
-    def __is_valid(
-        self, year: int | None, month: int | None, key_word: str | None
-    ) -> bool:
-        """
-        判断传入的年月是否比当前时间更早
-
-        :param year: 传入的年份
-        :param month: 传入的月份
-        :return: 如果传入的年月比当前时间更早，则返回 True；否则返回 False
-        """
-        if key_word:
-            return True
-        else:
-            current_date = datetime.now()
-
-            if year is None or month is None:
-                return False
-            if (year, month) == (2019, 4):
-                logger.warning("2019-4季度暂无数据")
-                return False
-            elif (year, month) < (2019, 1):
-                logger.warning("ANI Open项目仅支持2019年1月及其之后的数据")
-                return False
-            elif (year, month) > (current_date.year, current_date.month):
-                logger.warning("传入的年月晚于当前时间")
-                return False
-            else:
-                return True
-
     @property
     def __get_ani_season(self) -> str:
         """
-        根据 self.__year 和 self.__month 以及关键字 self.__key_word 判断更新的季度
+        根据 self.__year 和 self.__month 以及关键字 self.__key_word 返回更新的季度
         """
         if self.__key_word:
             return self.__key_word
 
         current_date = datetime.now()
-        if isinstance(self.__year, int) and isinstance(self.__month, int):
+        if self.__year and self.__month:
             year = self.__year
             month = self.__month
         else:
@@ -261,7 +250,7 @@ class Ani2Alist:
                 if resp.status != 200:
                     raise Exception(f"请求发送失败，状态码：{resp.status}")
                 feeds = parse(await resp.text())
-                
+
         for item in feeds.entries:
             rss_anime_dict[item.title] = [
                 convert_size_to_bytes(item.anime_size),
