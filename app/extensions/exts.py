967faac7addcc5483a2b98e2fb32f7e221cf9be6
Akimio521 <akimio2333@gmail.com>
2024-07-26 20:23:14 +0800

    update:修改exts符合新AlistPath.suffix属性

--- app/extensions/exts.py
+++ app/extensions/exts.py
@@ -1,10 +1,10 @@
 from typing import Final
 
-VIDEO_EXTS: Final = frozenset(("mp4", "mkv", "flv", "avi", "wmv", "ts", "rmvb", "webm"))
-EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union(("strm",))
+VIDEO_EXTS: Final = frozenset((".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm"))
+EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union((".strm",))
 
-SUBTITLE_EXTS: Final = frozenset(("ass", "srt", "ssa", "sub"))
+SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub"))
 
-IMAGE_EXTS: Final = frozenset(("png", "jpg"))
+IMAGE_EXTS: Final = frozenset((".png", ".jpg"))
 
-NFO_EXTS: Final = frozenset(("nfo",))
+NFO_EXTS: Final = frozenset((".nfo",))
