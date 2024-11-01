0a92f321f0599c07ed375eafc209d04f11b7b52a
Akimio521 <akimio2333@gmail.com>
2024-11-01 19:41:34 +0800

    feat(logging): 增强日志功能，添加自定义格式化器和日期轮换文件处理器

--- app/core/log.py
+++ app/core/log.py
@@ -1,10 +1,99 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+from datetime import datetime
+from pathlib import Path
+
+import click
 
 from app.core.config import settings
+
+FMT = "%(prefix)s %(message)s"
+
+# 日志级别颜色映射
+LEVEL_WITH_COLOR = {
+    logging.DEBUG: lambda level_name: click.style(str(level_name), fg="blue"),
+    logging.INFO: lambda level_name: click.style(str(level_name), fg="green"),
+    logging.WARNING: lambda level_name: click.style(str(level_name), fg="yellow"),
+    logging.ERROR: lambda level_name: click.style(str(level_name), fg="red"),
+    logging.CRITICAL: lambda level_name: click.style(
+        str(level_name), fg="red", bold=True
+    ),
+}
+
+
+class CustomFormatter(logging.Formatter):
+    """
+    自定义日志输出格式
+
+    对 logging.LogRecord 增加一个属性 prefix，level + time
+    """
+
+    def __init__(self, file_formatter: bool = False, fmt: str = None) -> None:
+        """
+        :param file_formatter: 是否为文件格式化器
+        """
+
+        self.__file_formatter = file_formatter
+        super().__init__(fmt=fmt)
+
+    def format(self, record: logging.LogRecord) -> str:
+
+        if self.__file_formatter:  # 文件中不需要控制字
+            record.prefix = f"【{record.levelname}】"
+        else:  # 控制台需要控制字
+            record.prefix = LEVEL_WITH_COLOR[record.levelno](f"【{record.levelname}】")
+
+        # 最长的 CRITICAL 为 8 个字符，保留 1 个空格作为分隔符
+        separator = " " * (9 - len(record.levelname))
+        dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        record.prefix += f"{separator}{dt} |"
+        return super().format(record)
+
+
+class TRFileHandler(TimedRotatingFileHandler):
+    """
+    日期轮换文件处理器
+    """
+
+    def __init__(self, log_dir: Path, encoding: str = "utf-8") -> None:
+        self.log_dir = log_dir
+        super().__init__(
+            self.__get_log_filname(),
+            when="midnight",
+            interval=1,
+            backupCount=0,
+            encoding=encoding,
+        )
+
+    def doRollover(self) -> None:
+        """
+        在轮换日志文件时，更新日志文件路径
+        """
+
+        self.baseFilename = self.__get_log_filname()
+        super().doRollover()
+
+    def __get_log_filname(self) -> str:
+        """
+        根据当前日期生成日志文件路径
+        """
+
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        return (self.log_dir / f"{current_date}.log").as_posix()
+
+
+def get_filehandler(
+    log_dir: Path,
+    is_dev: bool = False,
+    encoding: str = "utf-8",
+) -> logging.FileHandler:
+    """
+    获取文件处理器
+    """
+    if is_dev:
+        return logging.FileHandler(log_dir / "dev.log", encoding=encoding)
+    else:
+        return TRFileHandler(log_dir, encoding=encoding)
 
 
 class LoggerManager:
@@ -16,32 +105,30 @@ class LoggerManager:
         """
         初始化 LoggerManager 对象
         """
-        self.__logger = logging.getLogger(__name__)
+
+        self.__logger = logging.getLogger(settings.APP_NAME)
         self.__logger.setLevel(logging.DEBUG)
-        __formatter = logging.Formatter(fmt="[%(levelname)s]%(asctime)s - %(message)s")
 
-        if settings.DEBUG:
-            ch_level = logging.DEBUG
-            fh_mode = "w"
-        else:
-            ch_level = logging.INFO
-            fh_mode = "a"
-
-        __console_handler = logging.StreamHandler()
-        __console_handler.setLevel(ch_level)
-        self.__logger.addHandler(__console_handler)
-        __console_handler.setFormatter(__formatter)
-
-        __file_handler = RotatingFileHandler(
-            filename=settings.LOG,
-            mode=fh_mode,
-            maxBytes=10 * 1024 * 1024,
-            backupCount=5,
-            encoding="utf-8",
+        console_formatter = CustomFormatter(
+            file_formatter=False,
+            fmt=FMT,
         )
-        __file_handler.setLevel(logging.INFO)
-        self.__logger.addHandler(__file_handler)
-        __file_handler.setFormatter(__formatter)
+        file_formatter = CustomFormatter(
+            file_formatter=True,
+            fmt=FMT,
+        )
+
+        level = logging.DEBUG if settings.DEBUG else logging.INFO
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+        console_handler.setFormatter(console_formatter)
+        self.__logger.addHandler(console_handler)
+
+        file_handler = get_filehandler(settings.LOG_DIR, settings.DEBUG)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(file_formatter)
+        self.__logger.addHandler(file_handler)
 
     def __log(self, method: str, msg: str, *args, **kwargs) -> None:
         """
