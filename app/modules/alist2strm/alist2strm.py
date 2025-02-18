65263e86f4a3cda665792dafc62878f9d85d956c
Makise42 <you2929926348@gmail.com>
2025-02-18 22:07:40 +0800

    修改:当后缀在other_ext里时会被下载而不是转换为strm

--- app/modules/alist2strm/alist2strm.py
+++ app/modules/alist2strm/alist2strm.py
@@ -202,9 +202,10 @@ class Alist2Strm:
                 relative_path = relative_path[1:]
             local_path = self.target_dir / relative_path
 
-        if path.suffix.lower() in VIDEO_EXTS:
+        ext = path.suffix.lower()
+        # 修改:当后缀在other_ext里时会被下载而不是转换为strm
+        if ext in VIDEO_EXTS and ext.lstrip('.') not in self.download_exts:
             local_path = local_path.with_suffix(".strm")
-
         return local_path
 
     async def __cleanup_local_files(self) -> None:
