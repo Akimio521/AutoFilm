d998eb73c207d0442521d55be4672f7a84977322
Akimio521 <akimio2333@gmail.com>
2025-03-21 15:30:58 +0800

    feat(exts.py): 添加 .mpg 视频文件后缀到支持的扩展列表

--- app/extensions/exts.py
+++ app/extensions/exts.py
@@ -1,9 +1,11 @@
 from typing import Final
 
-VIDEO_EXTS: Final = frozenset((".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm", "wmv"))
-EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union((".strm",))
+VIDEO_EXTS: Final = frozenset(
+    (".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm", "wmv", ".mpg")
+)  # 视频文件后缀
+EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union((".strm",))  # 扩展视频文件后缀
 
-SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub"))
+SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub"))  # 字幕文件后缀
 
 IMAGE_EXTS: Final = frozenset((".png", ".jpg"))
 
